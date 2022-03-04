ef762691dc6e69474e14acf3bd4cb7cb46519b07
jonasbarth <jb901@exeter.ac.uk>
2022-03-04 20:10:54 +0100

    make code on run pi * remove duplicate timetable manager bean

--- deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/AppConfig.java
+++ deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/AppConfig.java
@@ -3,13 +3,11 @@ package com.jobarth.deutsche.bahn.data.acquisition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan("com.jobarth.deutsche.bahn.data.acquisition")
+//@ComponentScan("com.jobarth.deutsche.bahn.data.acquisition")
 public class AppConfig {
 
-    @Bean
-    public TimetableManagerImpl timetableManager() {
-        return new TimetableManagerImpl();
-    }
+
 }
