1b55eb685844273bf97c4cfd5c53c4f8e21a705f
jonasbarth <jb901@exeter.ac.uk>
2022-07-19 20:12:25 +0200

    * extract constants for station and timetable server * add javadoc * add log message for server startup

--- deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/server/StationServer.java
+++ deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/server/StationServer.java
@@ -2,20 +2,29 @@ package com.jobarth.deutsche.bahn.data.acquisition.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+/**
+ * A service for serving the {@link TimetableServiceImpl}.
+ */
 @Component
 public class StationServer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(TimetableServer.class);
+    private static final int PORT = 5001;
 
     @Autowired
     private StationServiceImpl stationService;
 
     public void start() throws InterruptedException, IOException {
+        LOGGER.info("Starting StationServer on port {}", PORT);
         Server server = ServerBuilder
-                .forPort(5001)
+                .forPort(PORT)
                 .addService(stationService)
                 .build();
         server.start();
--- deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/server/TimetableServer.java
+++ deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/server/TimetableServer.java
@@ -2,20 +2,29 @@ package com.jobarth.deutsche.bahn.data.acquisition.server;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+/**
+ * A service for serving the {@link TimetableServiceImpl}
+ */
 @Component
 public class TimetableServer {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(TimetableServer.class);
+    private final static int PORT = 5000;
 
     @Autowired
     private TimetableServiceImpl timetableService;
 
     public void start() throws InterruptedException, IOException {
+        LOGGER.info("Starting TimetableServer on port {}", PORT);
         Server server = ServerBuilder
-                .forPort(5000)
+                .forPort(PORT)
                 .addService(timetableService)
                 .build();
         server.start();
