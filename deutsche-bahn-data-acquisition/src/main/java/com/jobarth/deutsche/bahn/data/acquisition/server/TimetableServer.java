31e574f5ad65971ca8fc418ed5016b34db018c05
jonasbarth <jb901@exeter.ac.uk>
2022-07-23 15:07:35 +0200

    * use executor for timetable rpc service

--- deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/server/TimetableServer.java
+++ deutsche-bahn-data-acquisition/src/main/java/com/jobarth/deutsche/bahn/data/acquisition/server/TimetableServer.java
@@ -1,5 +1,6 @@
 package com.jobarth.deutsche.bahn.data.acquisition.server;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import org.slf4j.Logger;
@@ -8,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A service for serving the {@link TimetableServiceImpl}
@@ -22,10 +27,18 @@ public class TimetableServer {
     private TimetableServiceImpl timetableService;
 
     public void start() throws InterruptedException, IOException {
+        ExecutorService executor = new ThreadPoolExecutor(100, 1000, 60L, TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                new ThreadFactoryBuilder()
+                        .setDaemon(true)
+                        .setNameFormat("deutsche-bahn-data-acquisition-executor-%")
+                        .build());
+
         LOGGER.info("Starting TimetableServer on port {}", PORT);
         Server server = ServerBuilder
                 .forPort(PORT)
                 .addService(timetableService)
+                .executor(executor)
                 .build();
         server.start();
         server.awaitTermination();
